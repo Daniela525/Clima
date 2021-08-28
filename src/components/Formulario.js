2ed8e5a60f52b90d1cba7ef8ea53a13061cf48b3
DAS <daniaristi525@hotmail.com>
2021-08-28 17:00:08 -0500

    subiendo los primeros archivos

---
+++ src/components/Formulario.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import Error from "./Error"
+import PropTypes from "prop-types";
+
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
+
+   
+    const [error, guardarError] = useState(false);
+
+    // extraer ciudad y pais
+    const { ciudad, pais  } = busqueda;
+
+    //funcion que coloca los elementos en el state
+    const handleChange = e => {
+        // actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        }); // codigo comun para guardar objetos
+    }
+
+    // Cuando el usuario da submit al formulario
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //Validar
+        if(ciudad.trim() === "" || pais.trim() === "" ){
+            guardarError(true);
+            return;
+        } 
+        guardarError(false);
+
+        // Hace que cuando da submit de la consulta
+        guardarConsultar(true);
+
+    }
+
+    return ( 
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <Error mensaje= "Ambos campos son obligatorios" /> : null}
+
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+            <div className="inputñfield col s12">
+                <select
+                    name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
+                >
+                    <option value="">-- Selecciones un país --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+
+                </select>
+                <label htmlFor="pais">País: </label>
+
+                <div className="input-field col s12">
+                    <input 
+                        type="submit"
+                        value="Buscar Clima"
+                        className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                    />
+                </div>
+            </div>
+        </form>
+     );
+}
+
+Formulario.propTypes = {
+    busqueda: PropTypes.object.isRequired,
+    guardarBusqueda: PropTypes.func.isRequired,
+    guardarConsultar: PropTypes.func.isRequired
+}
+ 
+export default Formulario;
